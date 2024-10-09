9226f7e2685978c0547d650b2c518a9a0beb4836
Maxim Styushin <makcimkos@gmail.com>
2024-10-09 22:08:04 +0400

    fix: request_id propagation

--- pkg/api/middleware.go
+++ pkg/api/middleware.go
@@ -10,6 +10,24 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type statusResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewStatusResponseWriter(w http.ResponseWriter) *statusResponseWriter {
+	return &statusResponseWriter{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
+}
+
+// WriteHeader assigns status code and header to ResponseWriter of statusResponseWriter object
+func (sw *statusResponseWriter) WriteHeader(statusCode int) {
+	sw.statusCode = statusCode
+	sw.ResponseWriter.WriteHeader(statusCode)
+}
+
 // Generate (if there is none) and store request_id both in the request context AND X-REQUEST-ID header
 func RequestIDLoggerMiddleware(r *mux.Router) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
@@ -17,12 +35,14 @@ func RequestIDLoggerMiddleware(r *mux.Router) mux.MiddlewareFunc {
 			var id string
 			if r.URL.Query().Has("request_id") {
 				id = r.URL.Query().Get("request_id")
+			} else if id = r.Header.Get("x-request-id"); id != "" {
+				w.Header().Set("x-request-id", id)
 			} else {
 				id = uuid.NewString()
-				ctx := context.WithValue(r.Context(), "request_id", id)
-				r = r.WithContext(ctx)
-				w.Header().Set("x-request-id", id)
 			}
+			ctx := context.WithValue(r.Context(), "request_id", id)
+			r = r.WithContext(ctx)
+			w.Header().Set("x-request-id", id)
 			next.ServeHTTP(w, r)
 		})
 	}
@@ -33,10 +53,12 @@ func LoggerMiddleware(r *mux.Router) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			start := time.Now()
+			sw := NewStatusResponseWriter(w)
 			defer func() {
 				log.Printf(
-					"method=%s time=%v request_id=%s url=%s://%s%s",
+					"method=%s code=%v time=%v request_id=%s url=%s://%s%s",
 					r.Method,
+					sw.statusCode,
 					time.Since(start),
 					r.Context().Value("request_id"),
 					r.URL.Scheme,
@@ -44,7 +66,7 @@ func LoggerMiddleware(r *mux.Router) mux.MiddlewareFunc {
 					r.RequestURI,
 				)
 			}()
-			next.ServeHTTP(w, r)
+			next.ServeHTTP(sw, r)
 		})
 	}
 }
